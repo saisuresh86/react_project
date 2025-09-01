b08eaa78652a2a1cbf397d813ee5d04c1a90c8d4
K <suresh.c.k@capgemini.com>
2025-09-01 18:11:33 +0530

    react code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateCertificate from './CreateCertificate';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Certificate Creation</h1>
+      <CreateCertificate />
+    </div>
+  );
+}
+
+export default App;
